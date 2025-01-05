a5cbf833ee396a0dd6a0f469559f6806332a3540
DangAnDo <d_do@utb.cz>
2025-01-05 16:21:15 +0100

    Zobrazí se kdo má dneska svátek Přidání dynamické změny -po kliknutí se zobrazí svátek ve dne

--- src/app/services/svatkyapi.service.ts
+++ src/app/services/svatkyapi.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class SvatkyapiService {
 
-  constructor() { }
+export class SvatkyapiService {
+  private baseUrl = 'https://svatkyapi.cz/api/day';
+
+  constructor(private http: HttpClient) {}
+
+  // Získat dnešní svátek
+  getTodayNameday(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+    // Reguest URL: https://svatkyapi.cz/api/day
+  }
+
+  // Získat svátek pro konkrétní datum
+  getNamedayForDate(date: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${date}`);
+    // Reguest URL: https://svatkyapi.cz/api/day/YYYY-MM-DD
+  }
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
+import { SvatkyapiService  } from '../services/svatkyapi.service';    // Import služby pro získání svátků
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, IonicModule],
 })
 export class Tab1Page {
+  nameday: string | null = null;
 
-  constructor() {}
+  constructor(private namedayService: SvatkyapiService ) {}
 
+  // Automaticky zobrazí dnešní svátky
+  ngOnInit() {
+    this.todayNameday();
+  }
+
+  // Načtění dnešních svátků
+  todayNameday() {
+    this.namedayService.getTodayNameday().subscribe(
+      (data: any) => {this.nameday = data.name; }
+    );
+  }
+
+  // Načtění svátky na základě zvoleného datumu
+  dateNameday(event: any) {
+    const selectedDate = new Date(event.detail.value);
+    const formattedDate = selectedDate.toISOString().split('T')[0]; // Formát 2025-01-05T01:20:00.000Z -> 2025-01-05
+
+    this.namedayService.getNamedayForDate(formattedDate).subscribe(
+      (data: any) => { this.nameday = data.name; }
+    );
+  }
 }
