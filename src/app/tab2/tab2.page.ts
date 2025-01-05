c761dd9c91ac4f4a35a3dc105edc843f1f12a5ce
DangAnDo <d_do@utb.cz>
2025-01-05 19:03:45 +0100

    1. část: Vyhledávání podle data

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { SvatkyapiService } from '../services/svatkyapi.service';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, FormsModule, IonicModule],
 })
+
 export class Tab2Page {
+  inputDate: string = ''; // Uživatelem zadané datum
+  result: string | null = null; // Výsledek vyhledávání
 
-  constructor() {}
+  constructor(private svatkyapiService: SvatkyapiService) {}
 
+  // Vyhledávání podle datumu
+  searchByDate() {
+    // Zkontroluj, zda uživatel zadal datum
+    if (!this.inputDate) {
+      this.result = 'Zadejte prosím datum.';
+      return;
+    }
+  
+    // Zavolej službu pro získání svátku na zadané datum
+    this.svatkyapiService.getNamedayForDate(this.inputDate).subscribe({
+      next: (data: any) => { this.result = data.name ? data.name : 'Jméno nenalezeno pro zadané datum.'; },   // Vrátí jméno nebo chybovou hlášku
+
+      error: (err: any) => { this.result = 'Datum není podle formátu.'; },                                    // V případě chyby zobrazí chybovou hlášku
+    });
+  }
 }
+  
