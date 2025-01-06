41e0675af547d2a1435e5bfbdfe4a1c70d311b11
DangAnDo <d_do@utb.cz>
2025-01-06 16:24:35 +0100

    Vyřešený výpis oblíbených pro tab3

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { IonicModule } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, IonicModule],
 })
+
 export class Tab3Page {
+  favoriteNames: string[] = [];                  // Pole oblíbených jmen
 
-  constructor() {}
+  constructor(private storage: Storage) {}
 
+  async ngOnInit() {
+    await this.initStorage();
+    this.loadFavorites();
+  }
+
+  private async initStorage() {
+    await this.storage.create();
+  }
+
+  private async loadFavorites() {
+    const favorites = await this.storage.get('favorites');
+    this.favoriteNames = favorites || [];
+  }
 }
