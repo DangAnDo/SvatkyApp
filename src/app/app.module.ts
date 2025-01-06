23de4aa5ae5ae03bfdec770678b646817221dba2
DangAnDo <d_do@utb.cz>
2025-01-06 15:35:29 +0100

    přidání storage-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { IonicModule } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,6 +13,7 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    IonicStorageModule.forRoot(),     // Přidání Ionic Storage
   ],
   providers: [
     provideHttpClient(withInterceptorsFromDi()), // Nastavení HttpClient pro API
