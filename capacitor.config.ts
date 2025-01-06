4941aa39f5f3f65866dde1a129943140cbce4fbb
DangAnDo <d_do@utb.cz>
2025-01-07 00:33:37 +0100

    Změna na vlastní ikonku - ale splash nefunguje

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'SvatkyApp',
-  webDir: 'www'
+  webDir: 'www',
+
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Doba zobrazení splashscreenu v milisekundách
+      launchAutoHide: true, // Automatické skrytí splashscreenu
+      backgroundColor: '#ffffffff', // Barva pozadí (RGBA formát)
+      androidSplashResourceName: 'splash', // Název souboru pro Android
+      iosSplashResourceName: 'Default', // Název souboru pro iOS
+      showSpinner: true, // Zobrazit spinner během načítání
+      spinnerColor: '#000000', // Barva spinneru
+    },
+  },
 };
 
 export default config;
